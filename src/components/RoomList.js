abeab559c8ded6a4b218dc7bb4fe21749c938001
Kwena Mokgohlwa <kwena@Kwenas-MacBook-Air.local>
2018-02-26 00:21:01 +0200

    Checkpoint Create Chat Rooms Complete

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -4,7 +4,8 @@ class RoomList extends Component {
   constructor(props){
     super(props);
     this.state = {
-      rooms: []
+      rooms: [],
+      newRoomName: "",
     };
 
     this.roomsRef = this.props.firebase.database().ref('rooms');
@@ -14,8 +15,17 @@ class RoomList extends Component {
     this.roomsRef.on('child_added', snapshot => {
       const room = snapshot.val();
       room.key = snapshot.key;
+      this.setState({ room: room });
       this.setState({ rooms: this.state.rooms.concat( room ) });
     });
+  }
+
+  handleChange(e) {
+    this.setState({ newRoomName: e.target.value });
+  }
+
+  handleClick() {
+    this.roomsRef.child(this.state.rooms.length + 1).set({name: this.state.newRoomName});
   }
 
   componentWillUnmount() {
@@ -26,13 +36,23 @@ class RoomList extends Component {
     console.log(this.state.rooms);
     return (
       <aside>
-      {
-        this.state.rooms.map( (room, index) =>
-          <div className="chat-rooms" key={room.key}>
-            {room.name}
-          </div>
-        )
-      }
+        <div className="bloc-chat-header">
+          <h2>Bloc Chat</h2>
+          <input
+            type="text"
+            onChange={(e) => this.handleChange(e)}
+          />
+          <button onClick={() => this.handleClick()}>
+            Create Chat Room
+          </button>
+        </div>
+        {
+          this.state.rooms.map( (room, index) =>
+            <div className="chat-rooms" key={room.key}>
+              {room.name}
+            </div>
+          )
+        }
       </aside>
     );
   }
